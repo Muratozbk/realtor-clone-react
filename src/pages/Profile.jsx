5cf8d1b088ed825732a08a09a8233ba9f0b4b18e
muratozbk <slipstream11@yandex.com>
2023-08-13 12:35:34 +0300

    edit change functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,14 @@
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile } from 'firebase/auth';
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router';
+import { doc, setDoc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase'
+import { toast } from "react-toastify"
 
 export default function Profile() {
     const auth = getAuth()
     const navigate = useNavigate()
+    const [changeDetail, setChangeDetail] = useState(false)
     const [formData, setFormData] = useState({
         name: auth.currentUser.displayName,
         email: auth.currentUser.email
@@ -16,6 +20,30 @@ export default function Profile() {
         navigate('/')
     }
 
+    function onChange(e) {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value
+        }))
+    }
+
+    async function onSubmit() {
+        try {
+            if (auth.currentUser.displayName !== name) {
+                await updateProfile(auth.currentUser, {
+                    displayName: name
+                });
+                const docRef = doc(db, "users", auth.currentUser.uid)
+                await updateDoc(docRef, {
+                    name
+                })
+            }
+            toast.success("Profile details updated")
+        } catch (error) {
+            toast.error('Could not update the profile details')
+        }
+    }
+
     return (
         <>
             <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -24,17 +52,26 @@ export default function Profile() {
                     <form >
                         <input type="text"
                             value={name} id="name"
-                            disabled
-                            className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6'
+                            disabled={!changeDetail}
+                            onChange={onChange}
+                            className={`w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6 ${changeDetail && "bg-red-200 focus:bg-red-200"}`}
                         />
                         <input type="email"
                             value={email} id="email"
-                            disabled
-                            className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6 '
+                            disabled={!changeDetail}
+                            onChange={onChange}
+                            className={`w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6 ${changeDetail && "bg-red-200 focus:bg-red-200"}`}
                         />
                         <div className='flex justify-between whitespace-nowrap  sm:text-lg mb-6'>
                             <p className='flex items-center'>Do you want to change your name?
-                                <span className='text-red-600 hover:text-red-700 cursor-pointer transition ease-in-out duration-200 ml-1 font-semibold'> Edit</span>
+                                <span
+                                    onClick={() => {
+                                        changeDetail && onSubmit();
+                                        setChangeDetail((prevState) => !prevState)
+                                    }}
+                                    className='text-red-600 hover:text-red-700 cursor-pointer transition ease-in-out duration-200 ml-1 font-semibold'>
+                                    {changeDetail ? "Apply change" : "Edit"}
+                                </span>
                             </p>
                             <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer font-semibold'>Sign out</p>
                         </div>
