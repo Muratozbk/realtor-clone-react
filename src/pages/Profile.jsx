40032fdde6bd9bafeacf2da572e0f11fb582348b
muratozbk <slipstream11@yandex.com>
2023-08-14 16:28:15 +0300

    name section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -80,7 +80,7 @@ export default function Profile() {
                     </form>
                     <button type="submit"
                         className='w-full bg-blue-500 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-600 transition ease-in-out duration-150 hover:shadow-lg active:bg-blue-700'>
-                        <Link to='create-listing' className='flex justify-center items-center'>
+                        <Link to='/create-listing' className='flex justify-center items-center'>
                             <FcHome className='mr-2 text-3xl bg-red-200 rounded-full p-1 border-2' />
                             Sell or rent your home
                         </Link>
