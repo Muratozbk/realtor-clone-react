8b2f9739aa07ac947c139d1a5064c81a229c3f1a
muratozbk <slipstream11@yandex.com>
2023-08-15 22:15:09 +0300

    onChange onSubmit functions

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
+import Spinner from '../components/Spinner'
+import { toast } from 'react-toastify'
 
 export default function CreateListing() {
+    const [geolocationEnabled, setGeolocationEnabled] = useState(true)
+    const [loading, setLoading] = useState(false)
     const [formData, setFormData] = useState({
         type: "rent",
         name: "",
@@ -13,10 +17,59 @@ export default function CreateListing() {
         offer: false,
         regularPrice: 0,
         discountedPrice: 0,
+        latitude: 0,
+        longitude: 0,
+        images: {},
     });
-    const { type, name, description, address, bedrooms, bathrooms, parking, furnished, offer, regularPrice, discountedPrice } = formData;
-    function onChange() {
+    const { type, name, description, address, bedrooms, bathrooms, parking, furnished, offer, images, regularPrice, discountedPrice, latitude, longitude } = formData;
 
+    function onChange(e) {
+        let boolean = null;
+        if (e.target.value === "true") {
+            boolean = true;
+        }
+        if (e.target.value === "false") {
+            boolean = false
+        }
+        if (e.target.files) {
+            setFormData((prevState) => ({
+                ...prevState,
+                images: e.target.files
+            }))
+        }
+        if (!e.target.files) {
+            setFormData((prevState) => ({
+                ...prevState,
+                [e.target.id]: boolean ?? e.target.value,
+            }))
+        }
+    }
+
+    async function onSubmit(e) {
+        e.preventDefault();
+        setLoading(true);
+        if (discountedPrice >= regularPrice) {
+            setLoading(false)
+            toast.error('Discounted price must be less than regular price')
+            return;
+        }
+        if (images.length > 6) {
+            setLoading(false)
+            toast.error('Maximum 6 images are allowed')
+            return;
+        }
+        let geolocation = {};
+        let location;
+        if (geolocationEnabled) {
+            const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`);
+            const data = await response.json();
+            console.log(data)
+        }
+
+    }
+
+    if (loading) {
+        return <Spinner />
     }
 
     return (
@@ -25,7 +78,7 @@ export default function CreateListing() {
                 Create a Listing
             </h1>
 
-            <form >
+            <form onSubmit={onSubmit} >
                 <p className='text-lg mt-6 font-semibold'>Sell / Rent</p>
                 <div className="flex">
                     <button type="button" id='type'
@@ -34,7 +87,7 @@ export default function CreateListing() {
                         sell
                     </button>
                     <button type="button" id='type'
-                        value="sale" onClick={onChange}
+                        value="rent" onClick={onChange}
                         className={`ml-3 px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full ${type === "sale" ? "bg-white text-black" : "bg-slate-600 text-white"}`}>
                         rent
                     </button>
@@ -92,6 +145,28 @@ export default function CreateListing() {
                     value={address} onChange={onChange}
                     placeholder='Address' required
                     className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-4' />
+                {!geolocationEnabled && (
+                    <div className="flex space-x-6 justify-start mb-6">
+                        <div className="">
+                            <p className='text-lg font-semibold'>Latitude</p>
+                            <input type="number" id="latitude"
+                                value={latitude}
+                                onChange={onChange}
+                                required
+                                min={"-90"} max={"90"}
+                                className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center' />
+                        </div>
+                        <div className="">
+                            <p className='text-lg font-semibold'>Longitude</p>
+                            <input type="number" id="longitude"
+                                value={longitude}
+                                onChange={onChange}
+                                required
+                                min={"-180"} max={"180"}
+                                className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:text-gray-700 focus:border-slate-600 text-center' />
+                        </div>
+                    </div>
+                )}
                 <p className='text-lg font-semibold'>Description</p>
                 <textarea type="text" id="description"
                     value={description} onChange={onChange}
@@ -111,7 +186,7 @@ export default function CreateListing() {
                         No
                     </button>
                 </div>
-                {/* 1:18:35 */}
+
                 <div className="flex items-center mb-6">
                     <div className="">
                         <p className='text-lg font-semibold'>Regular price</p>
@@ -132,7 +207,7 @@ export default function CreateListing() {
                         <div className="">
                             <p className='text-lg font-semibold'>Discounted price</p>
                             <div className="flex w-full justify-center items-center space-x-6">
-                                <input type="number" id='regularPrice'
+                                <input type="number" id='discountedPrice'
                                     value={discountedPrice}
                                     onChange={onChange} min={"50"} max={"999999999"} required={offer}
                                     className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 text-center' />
