f86c4ea8a7292df6b55d8fbe894e27db98b850c9
muratozbk <slipstream11@yandex.com>
2023-08-17 21:49:22 +0300

    listingItem

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Moment from 'react-moment'
+import { MdLocationOn } from 'react-icons/md'
 
 export default function ListingItem({ listing, id }) {
+    console.log(listing.data)
     return (
-        <div> {listing.name}</div>
+        <li>
+            <Link to={`/category/${listing.type}/${id}`}>
+                <img src={listing.imgUrls[0]} alt="" />
+            </Link>
+            <Moment fromNow>
+                {listing.timestamp?.toData()}
+            </Moment>
+            <div className="">
+                <div className="">
+                    <MdLocationOn />
+                    <p>{listing.address} </p>
+                </div>
+                <p>{listing.name} </p>
+                {/* v-8  18:00 firebase not work */}
+            </div>
+        </li>
     )
 }
