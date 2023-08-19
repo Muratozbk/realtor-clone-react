f0318a5526b0e70d3a437a8ff2e629540c4b8537
muratozbk <slipstream11@yandex.com>
2023-08-19 21:46:55 +0300

    complete the listingitem component

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -5,8 +5,8 @@ import { MdLocationOn } from 'react-icons/md'
 
 export default function ListingItem({ listing, id }) {
     return (
-        <li className='relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition duration-150 ease-in-out'>
-            <Link to={`/category/${listing.type}/${id}`}>
+        <li className='relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition duration-150 ease-in-out m-2.5'>
+            <Link className='contents' to={`/category/${listing.type}/${id}`}>
                 <img className='h-[170px] w-full object-cover hover:scale-110 transition duration-200 ease-in-out'
                     loading='lazy'
                     src={listing.imgUrls[0]} alt="listing" />
@@ -15,24 +15,27 @@ export default function ListingItem({ listing, id }) {
                 className='absolute top-2 left-2 bg-[#3377cc] text-white uppercase font-semibold rounded-md px-2 py-1 shadow-lg'>
                 {listing.timestamp?.toDate()}
             </Moment >
-            <div className="w-full">
-                {/* 34:00 */}
-                <div className="">
-                    <MdLocationOn />
-                    <p>{listing.address} </p>
+            <div className="w-full p-2.5 ">
+
+                <div className="flex items-center space-x-1">
+                    <MdLocationOn className='h-4 w-4 text-green-600' />
+                    <p className='font-semibold text-sm mb-[2px] text-gray-600 truncate'>{listing.address} </p>
                 </div>
-                <p>{listing.name} </p>
-                <p>${listing.offer ? listing.discountedPrice.toString()
-                    .replace(/\B(?=(d{3})+(?!\d))/g, ",") : listing.regularPrice.toString()
-                        .replace(/\B(?=(d{3})+(?!\d))/g, ",")} </p>
+                <p className='font-semibold m-0 text-xl truncate'>
+                    {listing.name} </p>
+                <p className='text-[#457b9d] mt-2 font-semibold '>$
+                    {listing.offer ? listing.discountedPrice.toString()
+                        .replace(/\B(?=(d{3})+(?!\d))/g, ",") : listing.regularPrice.toString()
+                            .replace(/\B(?=(d{3})+(?!\d))/g, ",")} </p>
                 {listing.type === "rent" && " / month"}
-                <div className="">
-                    <div className="">
-                        <p>{listing.bedrooms > 1 ? `${listing.bedrooms} Beds ` : "1 Bed"} </p>
+                <div className="flex items-center mt-[10px] space-x-3">
+                    <div className="flex items-center space-x-1">
+                        <p className='font-semibold text-xs '>
+                            {listing.bedrooms > 1 ? `${listing.bedrooms} Beds ` : "1 Bed"} </p>
                     </div>
-                    <div className="">
-                        <p>{listing.bathrooms > 1 ? `${listing.bathrooms} Baths ` : "1 Bath"} </p>
-                        {/* v-8 26:00 */}
+                    <div className="flex items-center space-x-1">
+                        <p className='font-bold text-xs'>{listing.bathrooms > 1 ? `${listing.bathrooms} Baths ` : "1 Bath"} </p>
+
                     </div>
                 </div>
             </div>
